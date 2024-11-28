160a1df7bde8f5be785e215cd809d44c807b5534
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-28 13:34:49 +0600

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
+import sendResponse from './app/utils/sendResponse';
 
 const app: Application = express();
 
@@ -15,9 +16,11 @@ app.use('/api/v1', router);
 
 // Health Check
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send({
-    status: true,
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
     message: '🌐 University Server is live 🚀',
+    data: null,
   });
 });
 
