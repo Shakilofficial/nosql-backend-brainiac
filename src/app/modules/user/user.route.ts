d8830c0a360c8c45c10bcf64fe9bfc7c4eb93915
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-12-01 22:25:33 +0600

    create validateRequest middleware for validate the request body data from user

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { createStudentValidationSchema } from '../student/student.validation';
 
 const router = express.Router();
 
-router.post('/create-student', UserControllers.createStudent);
+router.post(
+  '/create-student',
+  validateRequest(createStudentValidationSchema),
+  UserControllers.createStudent,
+);
 router.post('/create-faculty');
 router.post('/create-admin');
 
