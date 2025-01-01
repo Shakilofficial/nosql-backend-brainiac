6067b5b7894fa0a6d7ebd82f97bba938bed22d92
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2025-01-01 21:39:13 +0600

    update offered course routes

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../user/user.constant';
 import { OfferedCourseControllers } from './offeredCourse.controller';
 import { OfferedCourseValidations } from './offeredCourse.validation';
 
@@ -7,16 +9,44 @@ const router = express.Router();
 
 router.post(
   '/create-offered-course',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
   OfferedCourseControllers.createOfferedCourse,
 );
-router.get('/', OfferedCourseControllers.getAllOfferedCourses);
-router.get('/:id', OfferedCourseControllers.getSingleOfferedCourse);
+
+router.get(
+  '/',
+  auth(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  OfferedCourseControllers.getAllOfferedCourses,
+);
+
+router.get(
+  '/:id',
+  auth(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  OfferedCourseControllers.getSingleOfferedCourse,
+);
+
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
   OfferedCourseControllers.updateOfferedCourse,
 );
-router.delete('/:id', OfferedCourseControllers.deleteOfferedCourse);
+
+router.delete(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  OfferedCourseControllers.deleteOfferedCourse,
+);
 
 export const OfferedCourseRoutes = router;
